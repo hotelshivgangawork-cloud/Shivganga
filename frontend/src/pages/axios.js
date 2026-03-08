6ef85d63eb95242584e73d88e353d674ea30d5e6
tanisha-6 <tanishashinde65@gmail.com>
2026-03-09 00:56:21 +0530

    final2

--- frontend/src/pages/axios.js
+++ frontend/src/pages/axios.js
@@ -12,7 +12,7 @@
 
 import axios from "axios";
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `/api`,
   withCredentials: false,
   headers: {
     "Content-Type": "application/json",
